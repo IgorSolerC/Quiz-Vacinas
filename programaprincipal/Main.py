53b4103eee875b87db1f3f3d212c1f19c9c51f5b
Igor Soler <49961203+IguanaSolerdo@users.noreply.github.com>
2020-11-19 01:48:48 -0300

    Update Main.py

--- programaprincipal/Main.py
+++ programaprincipal/Main.py
@@ -1,18 +1,17 @@
 from regrasquiz import Questao
 
-
 def menuInicial():
-    print("\n+--------------------------------------------------------------+\n" +
-          "|               QUIZ SOBRE VACINAÇÃO                             |" +
-          "\n+--------------------------------------------------------------+\n" +
-          "| Seja bem vindo ao quiz. Aqui o nosso objetivo é                |\n" +
-          "| espalhar conhecimento sobre um assunto importe                 |\n" +
-          "| mas que está em torno de muitas fake news.                     |\n" +
-          "| Vacinação                                                      |\n" +
-          "| Ao longo do questionário, para a alternativa correta responda: |\n" +
-          "| A              B               C               D             E |\n" +
-          "+----------------------------------------------------------------+", end="")
-
+    print("\n+---------------------------------------------------+\n" +
+          "|                QUIZ SOBRE VACINAÇÃO               |" +
+          "\n+---------------------------------------------------+\n" +
+          "| Seja bem vindo ao quiz. Aqui o nosso objetivo é   |\n" +
+          "| espalhar conhecimento sobre um assunto importe    |\n" +
+          "| mas que está em torno de muitas fake news.        |\n" +
+          "| Vacinação                                         |\n" +
+          "| Ao longo do questionário, para a alternativa      |\n" +
+          "| correta responda: A, B, C, D ou E                 |\n" +
+          "+---------------------------------------------------+", end="")
+    input("\n\nAperte 'ENTER' para prosseguir.")
 
 menuInicial()
 
